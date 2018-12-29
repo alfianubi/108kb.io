36e3de748a9c30d796a2a963358ffc9d77094cd3
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2018-12-30 06:18:37 +0700

    add option to open my blog directly via cli

--- cli.js
+++ cli.js
@@ -2,6 +2,13 @@
 
 const opn = require('opn')
 
+const getParams = process.argv[2]
+const opts = { wait: false }
+
 // Sorry to track you
 
-opn('https://108kb.io/?utm_source=npx', { wait: false })
+if (getParams === 'blog' || getParams === '--blog') {
+  opn('https://108kb.io/blog/?utm_source=npx', opts)
+} else {
+  opn('https://108kb.io/?utm_source=npx', opts)
+}
